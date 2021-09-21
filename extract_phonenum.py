2cd4faed04fbe002e5f8ba33c83e33dbf492369a
Ram <ramayyala@gmail.com>
2021-09-20 19:48:24 -0700

    histogram.py

--- extract_phonenum.py
+++ extract_phonenum.py
@@ -20,11 +20,12 @@ filename = sys.argv[1]
 #opens and reads text file to a string
 text_file = open(filename, "r")
 text = text_file.read()
+text_file.close()
 ##extract numbers from text file into a list
 matches=[]
 for match in pn.PhoneNumberMatcher(text, "US"):
     matches.append(match)
-##format numbers to correct output 
+##format numbers to correct output
 for numbers in matches:
     if pn.format_number(numbers.number, pn.PhoneNumberFormat.E164)[0:2]=='+1': ##number is national
         test=re.split(' |-',pn.format_number(matches[1].number, pn.PhoneNumberFormat.NATIONAL))
---
+++ histogram.py
@@ -0,0 +1,29 @@
+import argparse
+import seaborn as sns
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+##Parse arguments given and check if -f arg is included
+## if -f arg included, set index to whatever value given in -f input
+if len(sys.argv) == 3:
+    parser = argparse.ArgumentParser(description='Column Choice')
+    # Positional mandatory arguments
+    parser.add_argument('input_file',help='input tsv')
+    # Optional arguments
+    parser.add_argument('-f', dest='column', type=int, help='Column Choice')
+    args = parser.parse_args()
+    index=args.column
+#if -f arg not included, set index to 2 by default
+else:
+    index=2
+#CHECKS:
+#check to see if index is inbewteen acceptable range of column indexes: 0-9.
+if index not in range(10):
+    print ("ERROR: Column Index Given is out of range, please choose value bewteen 0-9")
+    sys.exit()
+#load in data given in first input
+data=sys.argv[1]
+dataframe = pd.read_csv(data,sep='\t')
+#Plot histogram and save to histogram.png in directory running in 
+sns.histplot(data=dataframe, x=dataframe.iloc[:, index])
+plt.savefig('histogram.png')
